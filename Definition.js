6cd42c533e9463dd5d9adf86505521d50eb28b4c
Ricky <147160304+rickygiri@users.noreply.github.com>
2023-10-26 18:23:15 +0530

    Add files via upload
    My javascrit file

---
+++ Definition.js
@@ -0,0 +1,92 @@
+// Function Defination...
+
+/*
+function myfun(a,b){
+   return a * b;
+}
+
+
+let k = myfun(2,5);
+console.log(k)
+
+// Function Expression..
+
+let greet = function(){
+   return 'Happy Birthday';
+}
+
+console.log(greet())
+
+
+
+// Function are hosted..
+myfun();
+
+function myfun(){
+   console.log('Hello I Am Hosted')
+}
+
+/// Function Expression Are not hoisted
+var greet = function(){
+   console.log('Hello I am check')
+}
+
+greet()
+
+
+
+// Arrow Function Easy code and redable
+// ES5..
+var expression = function(){
+   // Code complex..But don't worry
+}
+
+
+//ES6...
+// const Arrow = () => {}...
+
+// One line statements...
+
+const Arrow = value => value;
+console.log(Arrow(23));
+
+
+// IIFE....(Self Invoking Function...)
+(function() {
+   let d = 'Ricky';
+   console.log(d)
+})()
+
+
+
+function greet(){
+   return 'Apna Time Kab Ayega Bro!';
+}
+
+let store = greet()
+console.log(store)
+
+
+
+
+function myFun(a,b){
+   
+   return a,b;
+}
+
+console.log(myFun.length)
+console.log(myFun.toString())
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
